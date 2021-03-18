76ac67c42f26dc71c0268bd30d004f7b5c7d5def
Jeremiah113099 <jhay.barcelona.jb@gmail.com>
2021-03-18 22:19:21 +0800

    Method7&8

--- Person.java
+++ Person.java
@@ -8,6 +8,8 @@ public class Person
     private int numD;
 	private String address;
 	private String work;
+	private int empSalary;
+	private int OT;
   
   //Method 1: Trisha Mae Mallari
      public void getName(String name)
@@ -72,5 +74,27 @@ public class Person
     public int showNumofD()
     {
         return numD;
+    }
+
+//Method 7: Jeremiah Barcelona
+    public void getEmpSalary(int empSalary)
+    {
+        this.empSalary = empSalary;
+    }
+    
+    public int showEmpSalary()
+    {
+        return empSalary;
+    }
+    
+    //Method 8: Jeremiah Barcelona
+    public void getOT(int OT)
+    {
+        this.OT = OT;
+    }
+    
+    public int showOT()
+    {
+        return OT;
     }
 }
