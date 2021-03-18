cb2f28157b7ebe65361c398cc7a3a5320e4af11b
TrishaMallari <79267294+TrishaMallari@users.noreply.github.com>
2021-03-18 21:23:42 +0800

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,29 @@
+package tsm3ecasestudy;
+
+public class Person 
+{
+    private String name;
+    private String gender;
+  
+  //Method 1: Trisha Mae Mallari
+     public void getName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String showName()
+    {
+        return name;
+    }
+    
+    //Method 2: Trisha Mae Mallari
+    public void getGender(String gender)
+    {
+        this.gender = gender;
+    }
+  
+    public String showGender()
+    {
+        return gender;
+    }
+}
