ffee33627922340821c868b4c89c25c474fc7603
Angelsoomin <angelsoomin27@gmail.com>
2021-03-18 21:44:11 +0800

    Method 3 & 4

--- Person.java
+++ Person.java
@@ -4,6 +4,8 @@ public class Person
 {
     private String name;
     private String gender;
+	private String address;
+	private String work;
   
   //Method 1: Trisha Mae Mallari
      public void getName(String name)
@@ -25,5 +27,27 @@ public class Person
     public String showGender()
     {
         return gender;
+		
+    }
+	//Method  3: Angel Adriano
+     public void getAdress(String address)
+    {
+        this.address = address;
+    }
+    
+    public String showAddress()
+    {
+        return address;
+    }
+    
+    //Method  4: Angel Adriano
+    public void getWork(String work)
+    {
+        this.work = work;
+    }
+    
+    public String showWork()
+    {
+        return work;
     }
 }
